e349d4582b6df2fd6b13ad80507077a06a8bfa59
Fernando da Silva Sousa <wild.nando@gmail.com>
2025-12-05 18:08:47 -0300

    Adiciona produtos ao request de criação de billing
    Inclui os detalhes dos produtos no array de dados enviados ao criar uma nova cobrança.

--- src/Clients/BillingClient.php
+++ src/Clients/BillingClient.php
@@ -55,9 +55,15 @@ class BillingClient extends Client
         $requestData = [
             'frequency' => $data->frequency,
             'methods' => $data->methods,
-            'products' => $data->products,
             'returnUrl' => $data->metadata->return_url,
             'completionUrl' => $data->metadata->completion_url,
+            'products' => array_map(fn($product) => [
+                'externalId' => $product->external_id,
+                'name' => $product->name,
+                'description' => $product->description,
+                'quantity' => $product->quantity,
+                'price' => $product->price
+            ], $data->products),
         ];
 
         if (isset($data->customer)) {
